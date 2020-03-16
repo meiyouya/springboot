7ce8dc6d8668336f443fc94d0e6caabbe8c8e6cc
lawliet-zhang <1259420947@qq.com>
2020-03-16 17:06:48 +0800

    1. spring log config

---
+++ springboot-log/src/main/java/com/zql/springboot/log/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.zql.springboot.log.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dell
+ */
+@RestController
+@Slf4j
+@RequestMapping("/index")
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        log.info("hello");
+        return "Hello";
+    }
+}
---
+++ springboot-log/src/test/java/com/zql/springboot/log/SpringbootLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zql.springboot.log;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
